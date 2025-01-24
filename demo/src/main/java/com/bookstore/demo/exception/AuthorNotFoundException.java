2cd86405ddf7547f1a4cbe2025d3e6b17be7c647
Shubham-devstack <shubham.vatsaa@gmail.com>
2025-01-24 10:54:49 +0800

    Delete demo directory

--- demo/src/main/java/com/bookstore/demo/exception/AuthorNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bookstore.demo.exception;
-
-public class AuthorNotFoundException extends RuntimeException {
-    public AuthorNotFoundException(String name) {
-        super(String.format("Author with name '%s' not found", name));
-    }
-}
--- demo/src/main/java/com/bookstore/demo/exception/InvalidISBNProvided.java
+++
@@ -1,7 +0,0 @@
-package com.bookstore.demo.exception;
-
-public class InvalidISBNProvided extends RuntimeException {
-    public InvalidISBNProvided(String isbn) {
-        super(String.format("Invalid ISBN '%s' provided", isbn));
-    }
-}
